1cb4a95caefb9e7e5685caa7c6fe3d1b3b16d5b0
ta-yamada <ta-yamada@another-ware.co.jp>
2024-10-31 17:05:15 +0900

    test commit4

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'demo'),
+      home: const MyHomePage(title: 'demoa'),
     );
   }
 }
